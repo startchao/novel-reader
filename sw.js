112d96526a38951d2a57f30c01dc620fb240dab9
startchao <start2sd@gmail.com>
2026-03-06 07:09:58 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,23 @@
-const CACHE='novel-reader-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE=‘novel-reader-v9’;
+self.addEventListener(‘install’,e=>{
+self.skipWaiting();
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll([’./’,’./index.html’])));
+});
+self.addEventListener(‘activate’,e=>{
+e.waitUntil(
+caches.keys().then(ks=>Promise.all(
+ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+))
+);
+self.clients.claim();
+});
+self.addEventListener(‘fetch’,e=>{
+// 永遠先嘗試網路，失敗才用快取
+e.respondWith(
+fetch(e.request).then(r=>{
+const clone=r.clone();
+caches.open(CACHE).then(c=>c.put(e.request,clone));
+return r;
+}).catch(()=>caches.match(e.request))
+);
+});
