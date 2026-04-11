30d27a4a815e5739cf42c8d0102566b51d320977
startchao <start2sd@gmail.com>
2026-04-11 13:51:18 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-// 清除所有快取並自我登出
-self.addEventListener(‘install’,()=>self.skipWaiting());
-self.addEventListener(‘activate’,e=>{
-e.waitUntil(
-caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k))))
-.then(()=>self.clients.claim())
-.then(()=>self.registration.unregister())
-);
-});
